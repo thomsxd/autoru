84022e49589f51f2c591a7451a90337af50d7466
magasov <77aushev@mail.ru>
2025-05-07 16:33:33 +0300

    header full

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 
 @Component({
