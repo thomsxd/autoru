fe6715f57cf7439c7d9d16b694cfcf0363e47ae0
magasov <77aushev@mail.ru>
2025-05-08 15:04:05 +0300

    logic sliders carts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   selector: 'app-main',
   imports: [NgFor],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
   products = [
@@ -18,9 +18,9 @@ export class MainComponent {
       images: [
         'https://avatars.mds.yandex.net/get-autoru-vos/4387583/d8f9005e82006f1f4fdc130a352d7eab/320x240',
         'https://avatars.mds.yandex.net/get-autoru-vos/2179995/0b893671d70114e66a1d47d572871bc3/832x624',
-        'https://avatars.mds.yandex.net/get-autoru-vos/5993563/ef7413bdd9925ade34f9971347d6ca82/832x624',
-        'https://avatars.mds.yandex.net/get-autoru-vos/4387583/d8f9005e82006f1f4fdc130a352d7eab/320x240'
-      ]
+      ],
+      activeImageIndex: 0,
+      intervalId: null as any
     },
     {
       id: 2,
@@ -29,24 +29,57 @@ export class MainComponent {
       year: 2020,
       mileage: '45 000 км',
       images: [
-        'https://avatars.mds.yandex.net/get-autoru-vos/4387583/d8f9005e82006f1f4fdc130a352d7eab/320x240',
-        'https://avatars.mds.yandex.net/get-autoru-vos/2179995/0b893671d70114e66a1d47d572871bc3/832x624',
-        'https://avatars.mds.yandex.net/get-autoru-vos/5993563/ef7413bdd9925ade34f9971347d6ca82/832x624',
-        'https://avatars.mds.yandex.net/get-autoru-vos/4387583/d8f9005e82006f1f4fdc130a352d7eab/320x240'
-      ]
+        'https://10.img.avito.st/image/1/1.WgCa_ra49umsVzTsor8rT-tf9O8kX3Th7Fr06ypX_uMs.3LSoHxHXog8b-FmfV8W-w6ay9UMoah5KlBmQ2ZW2Yx8',
+        'https://20.img.avito.st/image/1/1.f5QL_La40309VRF4edVl2npd0Xu1XVF1fVjRf7tV23e9.STGESAaMereoCNW3-Ek_YgrUV08L8BjiQBG4YYi8HUw',
+        'https://30.img.avito.st/image/1/1.agF-_ra4xuhIVwTtJr9pTw9fxO7AX0TgCFrE6s5XzuLI.r8i0a0Lfh-BgEBT5X8X4ISwPePPY6FsGK8KRAPCCypk',
+        'https://50.img.avito.st/image/1/1.VCMZ2ba4-MovcDrPPaAibGh4-syneHrCb336yKlw8MCv.6aTB4OSYRfB4BNy4TL8sPPGLiD7v0DK7qQ3feFFTILw'
+      ],
+      activeImageIndex: 0,
+      intervalId: null as any
     },
     {
       id: 3,
-      name: 'Audi A6',
+      name: 'Mercedes Aushev test Magasov Test test',
       price: '3 800 000 ₽',
       year: 2019,
       mileage: '60 000 км',
       images: [
-        'https://avatars.mds.yandex.net/get-autoru-vos/4387583/d8f9005e82006f1f4fdc130a352d7eab/320x240',
-        'https://avatars.mds.yandex.net/get-autoru-vos/2179995/0b893671d70114e66a1d47d572871bc3/832x624',
-        'https://avatars.mds.yandex.net/get-autoru-vos/5993563/ef7413bdd9925ade34f9971347d6ca82/832x624',
-        'https://avatars.mds.yandex.net/get-autoru-vos/4387583/d8f9005e82006f1f4fdc130a352d7eab/320x240'
-      ]
+        'https://avatars.mds.yandex.net/get-autoru-vos/2159521/03bdccaffa8c895ee66384d89648663f/832x624',
+        'https://avatars.mds.yandex.net/get-autoru-vos/1936112/0595d644acfef8ee621e245558aa4f18/1200x900n',
+        'https://avatars.mds.yandex.net/get-autoru-vos/2023939/c8756cf049e542c8921483dcbac28ff8/832x624',
+        'https://avatars.mds.yandex.net/get-autoru-vos/4402921/305cd4ca1a9b76b248c7693a4732fc20/832x624',
+        'https://avatars.mds.yandex.net/get-autoru-vos/2158055/cdc6fe1078634b10bbf2a259dc1eab79/832x624'
+      ],
+      activeImageIndex: 0,
+      intervalId: null as any
     }
   ];
+
+  startSlider(index: number) {
+    
+    this.stopSlider(index);
+    
+    
+    this.products[index].intervalId = setInterval(() => {
+      this.nextImage(index);
+    }, 2000); 
+  }
+
+  stopSlider(index: number) {
+    if (this.products[index].intervalId) {
+      clearInterval(this.products[index].intervalId);
+      this.products[index].intervalId = null;
+    }
+  }
+
+  nextImage(index: number) {
+    const product = this.products[index];
+    product.activeImageIndex = (product.activeImageIndex + 1) % product.images.length;
+  }
+
+  setActiveImage(productIndex: number, imageIndex: number) {
+    this.products[productIndex].activeImageIndex = imageIndex;
+    
+    this.startSlider(productIndex);
+  }
 }
