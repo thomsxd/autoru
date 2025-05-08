522eaffc8c7dbc36fe405f4c5a0ea6ba5fb2c249
magasov <77aushev@mail.ru>
2025-05-08 10:09:30 +0300

    create carpages

---
+++ src/app/carbrand/carbrand.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-carbrand',
+  imports: [RouterLink],
+  templateUrl: './carbrand.component.html',
+  styleUrl: './carbrand.component.scss'
+})
+export class CarbrandComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { CarbrandComponent } from "../carbrand/carbrand.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CarbrandComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
