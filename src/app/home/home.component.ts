ffb41c07dd245f44f2f31d461f92b0ed4b8af459
magasov <77aushev@mail.ru>
2025-05-08 13:30:43 +0300

    main page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { CarbrandComponent } from "../carbrand/carbrand.component";
+import { MainComponent } from "../main/main.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CarbrandComponent],
+  imports: [HeaderComponent, CarbrandComponent, MainComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
